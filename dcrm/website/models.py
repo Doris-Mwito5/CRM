3a44535ba077d0d93f55d7d635daa4c7f3164114
Doris-Mwito5 <kawiramwito5@gmail.com>
2024-04-26 12:26:34 +0300

    created models

--- dcrm/website/models.py
+++ dcrm/website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    created_at = models.DateTimeField(auto_created=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=50)
+    address = models.CharField(max_length=20)
+    city = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return(f"{self.first_name} {self.last_name}")
+    
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Customer
+
 
 def home(request):
+    customers = Customer.objects.all()
     #check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -18,7 +21,7 @@ def home(request):
             messages.success(request, "An error occured")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'customers':customers})
     
     
 def logout_user(request):
